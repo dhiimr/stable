be9eddd7f462f2ad36727a4e30fac232db241e28
Edi Susanto <51095316+dhiimr@users.noreply.github.com>
2023-09-18 20:02:45 +0700

    Update diff.sh

--- diff.sh
+++ diff.sh
@@ -50,8 +50,8 @@ aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://civitai.com/api/do
 cd /content/stable-diffusion-webui
 git reset --hard
 git -C /content/stable-diffusion-webui/repositories/stable-diffusion-stability-ai reset --hard
-aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/stable-diffusion-2-inpainting/resolve/main/512-inpainting-ema.ckpt -d /content/stable-diffusion-webui/models/Stable-diffusion -o 512-inpainting-ema.ckpt
-aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/stable-diffusion-2-inpainting/raw/main/v2-inpainting-inference.yaml -d /content/stable-diffusion-webui/models/Stable-diffusion -o 512-inpainting-ema.yaml
+#aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/stable-diffusion-2-inpainting/resolve/main/512-inpainting-ema.ckpt -d /content/stable-diffusion-webui/models/Stable-diffusion -o 512-inpainting-ema.ckpt
+#aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/stable-diffusion-2-inpainting/raw/main/v2-inpainting-inference.yaml -d /content/stable-diffusion-webui/models/Stable-diffusion -o 512-inpainting-ema.yaml
 #cp /content/drive/MyDrive/SD/models/Stable-diffusion/realisticVisionV51_v13-inpainting.safetensors /content/stable-diffusion-webui/models/Stable-diffusion/
 #rsync -ah --progress /content/drive/MyDrive/SD/models/Stable-diffusion/realisticVisionV51_v13-inpainting.safetensors /content/stable-diffusion-webui/models/Stable-diffusion/
 gdown --id 107M-W8f11I4-sKwFqAKxxfS4Y412Vn3j -O /content/stable-diffusion-webui/models/Stable-diffusion/
